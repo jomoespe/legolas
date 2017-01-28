f1d91dfe4f75ff8fcf28d5e5c20b0d0f7e1c1a2a
Jose Moreno <jose.morenoesteban@zooplus.com>
2017-01-28 11:04:20 +0100

    Add Environment properties functions

---
+++ src/main/java/com/josemorenoesteban/lab/legolas/analysis/Environment.java
@@ -0,0 +1,29 @@
+package com.josemorenoesteban.lab.legolas.analysis;
+
+import static java.lang.System.getenv;
+import static java.lang.System.getProperty;
+import static java.util.Optional.ofNullable;
+
+import java.util.function.Function;
+import java.util.Optional;
+
+public class Environment {
+    private static final Function<String, Optional<String>> env = name -> 
+            ofNullable(getenv(name));
+    
+    private static final Function<String, Optional<String>> prop = name -> 
+            ofNullable(getProperty(name));
+    
+    public static final Function<String, Optional<String>> value = name -> {
+        Optional<String> envValue = env.apply(name);
+        return envValue.isPresent() ?  envValue : prop.apply(name);
+    };
+    
+    public static final Function<String, Optional<Integer>> asInteger = name -> 
+        value.apply(name)
+              .map(Integer::valueOf);
+
+    public static final Function<String, Optional<Float>> asFloat = name -> 
+        value.apply(name)
+              .map(Float::valueOf);
+}
---
+++ src/test/java/com/josemorenoesteban/lab/legolas/analysis/EnvironmentTest.java
@@ -0,0 +1,36 @@
+package com.josemorenoesteban.lab.legolas.analysis;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EnvironmentTest {
+    public static final String LEGOLAS_TEST_PROPERTY       = "LEGOLAS_TEST_PROPERTY";
+    public static final String LEGOLAS_TEST_PROPERTY_VALUE = "OK";
+    
+    @Before
+    public void setupSystemProperties() {
+        System.setProperty(LEGOLAS_TEST_PROPERTY, LEGOLAS_TEST_PROPERTY_VALUE);
+    }
+
+    @After
+    public void cleanupSystemProperties() {
+        System.clearProperty(LEGOLAS_TEST_PROPERTY);
+    }
+
+    @Test
+    public void canGetDefaultValue() {
+       final String OTHER_PROPERTY       = "OTHER_PROPERTY";
+       final String OTHER_PROPERTY_VALUE = "OTHER_PROPERTY_VALUE";
+       assertEquals(OTHER_PROPERTY_VALUE, Environment.value.apply(OTHER_PROPERTY).orElse(OTHER_PROPERTY_VALUE));
+    }
+
+
+    @Test
+    public void canGetSystemPropertyValue() {
+        assertEquals(LEGOLAS_TEST_PROPERTY_VALUE, Environment.value.apply(LEGOLAS_TEST_PROPERTY).orElse("5"));
+    }
+    
+}
